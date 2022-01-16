de6d20e1dc1d82177be936118dc72ec90b218317
Vid Barbaro <i459559@fhict.nl>
2022-01-16 21:26:32 +0100

    Add match tests

--- backend_football/src/test/java/com/individuals3/backend_football/domain/MatchTest.java
+++ backend_football/src/test/java/com/individuals3/backend_football/domain/MatchTest.java
@@ -2,6 +2,7 @@ package com.individuals3.backend_football.domain;
 
 import com.individuals3.backend_football.constant.Authority;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -51,49 +53,104 @@ class MatchTest {
     public static final boolean USER_IS_ACTIVE = true;
     public static final boolean USER_IS_NOT_LOCKED = true;
     User referee = new User(ID3, USER_ID, USER_FIRST_NAME, USER_LAST_NAME, USER_USERNAME, USER_PASSWORD, USER_EMAIL, USER_PROFILE_IMG_URL, USER_LAST_LOGIN_DATE, USER_LAST_LOGIN_DATE, USER_JOIN_DATE, USER_ROLE, USER_AUTHORITIES, USER_IS_ACTIVE, USER_IS_NOT_LOCKED);
-    
+
+    public static final LocalDateTime MATCH_DATE_TIME = LocalDateTime.now();
+    public static final String MATCH_LOCATION = "Location";
+    Match match = new Match(homeTeam, awayTeam, referee, MATCH_DATE_TIME, MATCH_LOCATION);
+
     @Test
     @Transactional
     void setId() {
+        // arrange
+
+        // act
+        match.setId(100L);
+
+        // assert
+        Assertions.assertEquals(100L, match.getId());
     }
 
     @Test
     @Transactional
     void setHomeTeamId() {
+        // arrange
+
+        // act
+        match.setHomeTeamId(awayTeam);
+
+        // assert
+        Assertions.assertEquals(awayTeam, match.getAwayTeamId());
     }
 
     @Test
     @Transactional
     void setAwayTeamId() {
-    }
+        // arrange
 
-    @Test
-    @Transactional
-    void setMatchDateTime() {
+        // act
+        match.setAwayTeamId(homeTeam);
+
+        // assert
+        Assertions.assertEquals(homeTeam, match.getAwayTeamId());
     }
 
     @Test
     @Transactional
     void setLocation() {
+        // arrange
+
+        // act
+        match.setLocation("New Location");
+
+        // assert
+        Assertions.assertEquals("New Location", match.getLocation());
     }
 
     @Test
     @Transactional
     void setRefereeId() {
+        // arrange
+
+        // act
+        match.setRefereeId(referee);
+
+        // assert
+        Assertions.assertEquals(referee, match.getRefereeId());
     }
 
     @Test
     @Transactional
     void setIsFinished() {
+        // arrange
+
+        // act
+        match.setIsFinished(false);
+
+        // assert
+        Assertions.assertEquals(false, match.getIsFinished());
     }
 
     @Test
     @Transactional
     void setHomeTeamGoals() {
+        // arrange
+
+        // act
+        match.setHomeTeamGoals(5);
+
+        // assert
+        Assertions.assertEquals(5, match.getHomeTeamGoals());
     }
 
     @Test
     @Transactional
     void setAwayTeamGoals() {
+        // arrange
+
+        // act
+        match.setAwayTeamGoals(2);
+
+        // assert
+        Assertions.assertEquals(2, match.getAwayTeamGoals());
     }
 }
